177f6fbbe1aa29d8c3bc572db06f722aa2fe152e
adolfo carneiro <adolfocarneiro555@gmail.com>
2026-04-29 08:54:27 -0300

    neoforge: register MVP comb items with placeholder assets

--- neoforge/src/main/java/com/curiousbees/neoforge/registry/ModItems.java
+++ neoforge/src/main/java/com/curiousbees/neoforge/registry/ModItems.java
@@ -19,6 +19,18 @@ public final class ModItems {
             ITEMS.register("bee_analyzer",
                     () -> new BeeAnalyzerItem(new Item.Properties().stacksTo(1)));
 
+    // --- Comb items ---
+    public static final DeferredHolder<Item, Item> MEADOW_COMB =
+            ITEMS.register("meadow_comb",    () -> new Item(new Item.Properties()));
+    public static final DeferredHolder<Item, Item> FOREST_COMB =
+            ITEMS.register("forest_comb",    () -> new Item(new Item.Properties()));
+    public static final DeferredHolder<Item, Item> ARID_COMB =
+            ITEMS.register("arid_comb",      () -> new Item(new Item.Properties()));
+    public static final DeferredHolder<Item, Item> CULTIVATED_COMB =
+            ITEMS.register("cultivated_comb",() -> new Item(new Item.Properties()));
+    public static final DeferredHolder<Item, Item> HARDY_COMB =
+            ITEMS.register("hardy_comb",     () -> new Item(new Item.Properties()));
+
     public static void register(IEventBus modEventBus) {
         ITEMS.register(modEventBus);
     }
