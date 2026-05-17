b82f8f7cac6130b09c13cb05333ad574fda2ba3a
adolfo carneiro <adolfocarneiro555@gmail.com>
2026-05-17 13:56:56 -0300

    chore: remove hand-written JSON replaced by datagen
    Delete tag JSONs (item/beehive_upgrades, item/centrifuge_upgrades,
    item/frames, minecraft/block/beehives, minecraft/poi/bee_home) that are
    now generated by CuriousBeesItemTagProvider and CuriousBeesBlockTagProvider.
    Apply spotless formatting to CuriousBeesItemTagProvider.

--- neoforge/src/main/java/com/curiousbees/neoforge/datagen/CuriousBeesItemTagProvider.java
+++ neoforge/src/main/java/com/curiousbees/neoforge/datagen/CuriousBeesItemTagProvider.java
@@ -6,8 +6,6 @@ import com.curiousbees.neoforge.registry.ModTags;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.PackOutput;
 import net.minecraft.data.tags.ItemTagsProvider;
-import net.minecraft.data.tags.TagsProvider;
-import net.minecraft.world.level.block.Block;
 import net.neoforged.neoforge.common.data.BlockTagsProvider;
 import net.neoforged.neoforge.common.data.ExistingFileHelper;
 
